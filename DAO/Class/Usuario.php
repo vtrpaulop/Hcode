9ffd5e555dbac0a024e695032e9c075f152c3142
vtrpaulop <vtrpaulop@gmail.com>
2024-10-09 18:58:46 -0300

    metodos de lista

--- DAO/Class/Usuario.php
+++ DAO/Class/Usuario.php
@@ -55,13 +55,41 @@ Class Usuario {
         }
     }
 
-    public function __tostring(){
-        return json_encode(array(
-            "idusuario" => $this->getIdusuario(),
-            "deslogin" => $this->getDeslogin(),
-            "dessenha" => $this->getDessenha(),
-            "dtcadastro" => $this->getDtcadastro()->format("d/m/Y H:i:s")
-        ));
+    public static function getList(){
+
+        $sql = new Sql();   
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
     }
+
+    public static function search($login){
+        $sql = new Sql();
+        $sql->select(" SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(":SEARCH"=>"%".$login."%"));
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(":SEARCH"=>"%".$login."%"));   
+    }
+
+    public function login($login, $password){
+
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password));
+        if (count($results) > 0) {
+            $row = $results[0];
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime( $row['dtcadastro']));
+        } else {
+            throw new Exception("Login e/ou senha inválidos");
+        }}
+    public function __tostring(){
+    return json_encode(array(
+        "idusuario" => $this->getIdusuario(),
+        "deslogin" => $this->getDeslogin(),
+        "dessenha" => $this->getDessenha(),
+        "dtcadastro" => ($this->getDtcadastro() instanceof DateTime) 
+                        ? $this->getDtcadastro()->format("d/m/Y H:i:s") 
+                        : null // ou algum valor padrão como "Data não disponível"
+    ));
+}
+
 }
 ?>
--- DAO/index.php
+++ DAO/index.php
@@ -1,12 +1,34 @@
 <?php
 
-require_once("config.php");
+require_once "config.php";
 
+
+echo "Carrega um usuário apenas: <br>";
 $root = new Usuario();
-
 $root->loadById(3);
+echo ($root). "<br>";
 
-echo $root;
+echo "<br>";
 
+echo "Carrega uma lista de usuarios: <br>";
 
-?>
+$lista = Usuario::getList();
+
+echo json_encode($lista). "<br>";
+
+echo "<br>";
+
+echo "Carrega uma lista de usuario buscando pelo login: <br>";
+
+$search = Usuario::search("j");
+
+echo json_encode($search). "<br><br>";
+
+echo "Carrega um usuario usando o login e a senha: <br>";
+
+$usuario = new Usuario();
+
+$usuario->login("root", "!@#$");
+
+echo $usuario;
+
