951617c41cabbb3003c174033f601ea6de4ea74f
vtrpaulop <vtrpaulop@gmail.com>
2024-10-12 13:56:34 -0300

    Movendo arquivos

---
+++ RENAME/exemplo-01.php
@@ -0,0 +1,34 @@
+<?php
+
+$dir1 = "folder01";
+$dir2 = "folder02";
+
+if(!is_dir($dir1)){
+    mkdir($dir1);
+}    
+
+if(!is_dir($dir2)){ 
+    mkdir($dir2);    
+}
+
+$filename = "README.txt";
+
+if(!file_exists($dir1 . DIRECTORY_SEPARATOR . "$filename")){
+    
+    $file = fopen($dir1 . DIRECTORY_SEPARATOR . "$filename", "w+");
+
+    fwrite($file, date("Y-m-d H:i:s"));
+
+    fclose($file);
+}
+
+rename (
+    $dir1 . DIRECTORY_SEPARATOR . "$filename", //origem
+    $dir2 . DIRECTORY_SEPARATOR . "$filename"  //destino
+
+); 
+
+        echo "Arquivo renomeado e movido com sucesso!";
+
+
+?>
