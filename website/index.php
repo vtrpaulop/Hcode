855911beab650910fff400c5e1fa26076f2ba039
vtrpaulop <vtrpaulop@gmail.com>
2024-10-17 10:21:44 -0300

    Slim Framework

--- website/index.php
+++ website/index.php
@@ -1,3 +1,22 @@
 <?php
 
-phpinfo();
+require_once ("vendor/autoload.php");
+
+$app = new \Slim\App();
+
+$app->get('/', function ($request, $response, $args) {
+    return $response->getBody()->write('Hello World!');
+});
+
+$app->get('/oi', function ($request, $response, $args) {
+    return $response->getBody()->write(json_encode(date('Y-m-d H:i:s')));
+});
+
+
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $name = $args['name'];
+    return $response->getBody()->write('Hello, ' . $name);
+});
+
+$app->run();
